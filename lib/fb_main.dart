678b6c4501b49b180672bbf2aea12b69d0fb237f
AmosPeterAlex <amospeteralex28@gmail.com>
2024-01-11 16:31:37 +0530

    commit

--- lib/fb_main.dart
+++ lib/fb_main.dart
@@ -1,3 +1,5 @@
+///This project is basically trying to recreate my home page of facebook
+///Helped in studying about new Widgets
 import 'package:fb_clone/home.dart';
 import 'package:flutter/material.dart';
 
